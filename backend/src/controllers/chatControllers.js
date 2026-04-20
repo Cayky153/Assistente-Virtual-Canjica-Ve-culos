0834aec845ca590e5c8b64e9a731e41fc9d88c4f
Cayky153 <antonio.cayky987@gmail.com>
2026-04-20 03:24:18 -0300

    fix:corrigido funcao

--- backend/src/controllers/chatControllers.js
+++ backend/src/controllers/chatControllers.js
@@ -1,30 +1,33 @@
-import {gerarResposta} from '../services/iaService.js' 
+import { gerarResposta } from '../services/iaService.js';
 
 export async function promptController(req, res) {
     try {
         if (!req.body)
             return res.status(400).json({ message: "Req.body vazio" });
-        if (!req.body.prompt)
-            return res.status(400).json({ message: "Req.body vazio" });
 
-        const prompt = req.body.prompt;
+        const { conversationId, prompt } = req.body;
 
-        if (prompt.length > 2000) {
-            return res.status(400).json({ message: "Prompt muito grande" });
-        }
-        if (typeof prompt !== 'string') {
+        if (!conversationId)
+            return res.status(400).json({ message: "conversationId obrigatório" });
+
+        if (typeof prompt !== 'string')
             return res.status(400).json({ message: "Prompt não é string" });
-        }
-        if (!prompt.trim()) {
-            return res.status(400).json({ message: "Prompt vazio" });
-        }
-        const response = await gerarResposta(from, message);
 
-        res.json(response);
+        if (!prompt.trim())
+            return res.status(400).json({ message: "Prompt vazio" });
+
+        if (prompt.length > 2000)
+            return res.status(400).json({ message: "Prompt muito grande" });
+
+        const response = await gerarResposta({
+            conversationId,
+            prompt
+        });
+
+        return res.json(response);
+
     } catch (err) {
         console.error("Erro no controller:", err);
         return res.status(500).json({ message: "Erro interno" });
     }
-
-
 }
