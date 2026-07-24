c25796213caf9972f78e951c4d03277a58ee13c9
Cayky153 <antonio.cayky987@gmail.com>
2026-07-24 14:00:17 -0300

    feat: adicionado limite de 1000 mensagens processadas

--- backend/src/services/messageTrackerService.js
+++ backend/src/services/messageTrackerService.js
@@ -1,1 +1,8 @@
 export const processedMessages = new Set();
+export function addProcessed(id) {
+    processedMessages.add(id);
+    if (processedMessages.size > 1000) {
+        const primeiro = processedMessages.values().next().value;
+        processedMessages.delete(primeiro);
+    }
+}
